55a8d0e82fc02daaa305da7ec05e454adfa8ce2d
Vadim Rastyagaev <rastyagaev@gmail.com>
2014-11-27 14:20:27 +0300

    Update uploadcare_helpers.rb

--- lib/uploadcare_helpers.rb
+++ lib/uploadcare_helpers.rb
@@ -1,8 +1,9 @@
 require "uploadcare_helpers/engine"
-require 'simple_form'
 require 'uploadcare_helpers/simple_form_inputs'
 
 module UploadcareHelpers
 end
 
-SimpleForm::Inputs.send :include, UploadcareHelpers::SimpleFormInputs
+if defined? SimpleForm
+  SimpleForm::Inputs.send :include, UploadcareHelpers::SimpleFormInputs
+end
